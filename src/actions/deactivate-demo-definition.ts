295a44f292d2f84139c989c32e6bfa74e61e2488
Peter Murray <681306+peter-murray@users.noreply.github.com>
2021-04-21 20:31:37 +0000

    Adding deactivation action step

---
+++ src/actions/deactivate-demo-definition.ts
@@ -0,0 +1,47 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { inspect } from 'util';
+import { GitHubDeploymentManager } from '../GitHubDeploymentManager';
+import { getOctokit, getRequiredInput } from '../util';
+
+async function run() {
+  try {
+    await exec();
+  } catch (err) {
+    core.debug(inspect(err))
+    core.setFailed(err);
+  }
+}
+run();
+
+
+async function exec() {
+  const inputs = {
+    environment_deployment_id: getRequiredInput('environment_deployment_id'),
+    demo_deployment_id: getRequiredInput('demo_deployment_id'),
+  };
+
+  const octokit = getOctokit();
+  const deploymentManager = new GitHubDeploymentManager(github.context.repo, octokit, github.context.ref);
+
+  const environment_id = parseInt(inputs.environment_deployment_id);
+  if (isNaN(environment_id)) {
+    throw new Error(`environment_deployment_id parameter '${inputs.environment_deployment_id}', is not a valid number`);
+  }
+  core.startGroup('Environment');
+  core.info(`Deactivating environment ${environment_id}`);
+  const envResult = await deploymentManager.deactivateAndDeleteDeployment(environment_id);
+  core.info(`deactivated? ${envResult}`);
+  core.endGroup();
+
+
+  const demo_deployment_id = parseInt(inputs.demo_deployment_id);
+  if (isNaN(demo_deployment_id)) {
+    throw new Error(`demo_deployment_id parameter '${inputs.demo_deployment_id}', is not a valid number`);
+  }
+  core.startGroup('Demo Deployment')
+  core.info(`Deactivating demo deployment ${demo_deployment_id}`);
+  const demoResult = await deploymentManager.deactivateAndDeleteDeployment(demo_deployment_id);
+  core.info(`deactivated? ${demoResult}`);
+  core.endGroup();
+}
